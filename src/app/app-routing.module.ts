2336ff48b1cc9384a774e8f223a8e163dbf4f85c
Dino Horvat <dxh3401@rit.edu>
2018-12-27 12:07:43 +0100

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,13 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {MainComponent} from './layout/main/main.component';
 import {LoginComponent} from './components/login/login.component';
+import {PlayingNowComponent} from './components/playing-now/playing-now.component';
+import {BackupMediaComponent} from './components/backup-media/backup-media.component';
+import {DeviceSetupComponent} from './components/device-setup/device-setup.component';
+import {AlbumsComponent} from './components/albums/albums.component';
+import {ExportMediaComponent} from './components/export-media/export-media.component';
+import {ImportMediaComponent} from './components/import-media/import-media.component';
+import {MediaManagerComponent} from './components/media-manager/media-manager.component';
 
 const routes: Routes = [
     {
@@ -13,7 +20,14 @@ const routes: Routes = [
         path: 'main',
         component: MainComponent,
         children: [
-          {path: 'login', component: LoginComponent}
+          {path: 'login', component: LoginComponent},
+          {path: 'albums', component: AlbumsComponent},
+          {path: 'backup', component: BackupMediaComponent},
+          {path: 'device', component: DeviceSetupComponent},
+          {path: 'export', component: ExportMediaComponent},
+          {path: 'import', component: ImportMediaComponent},
+          {path: 'media', component: MediaManagerComponent},
+          {path: 'playing', component: PlayingNowComponent},
         ]
     }
 ];
