73c83176d87189ef0a63efe3430127cdb06243c4
Dino Horvat <dxh3401@rit.edu>
2018-12-27 11:06:49 +0100

    navbar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {MainComponent} from './main/main.component';
 import {RouterModule} from '@angular/router';
+import {NavbarComponent} from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    NavbarComponent
   ],
   imports: [
     RouterModule
