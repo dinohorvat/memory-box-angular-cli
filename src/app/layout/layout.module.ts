b785c2394eea6dff9161f0186c6b6d615e132ca3
Dino Horvat <dxh3401@rit.edu>
2018-12-27 11:18:05 +0100

    sidebar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {MainComponent} from './main/main.component';
 import {RouterModule} from '@angular/router';
 import {NavbarComponent} from './navbar/navbar.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     MainComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     RouterModule
