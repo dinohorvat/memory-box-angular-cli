e0997cdb04a14b0b6975769e0bae987b21036ecd
Dino Horvat <dxh3401@rit.edu>
2018-12-28 21:40:06 +0100

    Backup Media

--- src/app/components/backup-media/backup-media.component.ts
+++ src/app/components/backup-media/backup-media.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {GlobalService} from '../../services/global.service';
+import {Router} from '@angular/router';
+import {FileService} from '../../services/file.service';
 
 @Component({
   selector: 'app-backup-media',
@@ -8,7 +11,28 @@ import {Component, OnInit} from '@angular/core';
 
 export class BackupMediaComponent implements OnInit {
 
-  ngOnInit(): void {
+  available_devices: any = [];
+  backupDesc;
+  backupSrc;
+  available = false;
+  constructor(private globalService: GlobalService, private router: Router,
+              private fileService: FileService) {}
 
+  ngOnInit(): void {
+    this.getDeviceList();
   }
+
+  getDeviceList() {
+    this.globalService.getDeviceList().subscribe((res) => {
+      this.available_devices = res;
+      if (this.available_devices.length > 1) {
+        this.backupSrc = this.available_devices[0].diskPath;
+        this.backupDesc = this.available_devices[1].diskPath;
+        this.available = true;
+      } else {
+        this.available = false;
+      }
+    });
+  }
+
 }
