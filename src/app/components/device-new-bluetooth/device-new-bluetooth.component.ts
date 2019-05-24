d6fa58326a9f8b91449cf8d146fcd21deb40f7bb
Dino Horvat <dxh3401@rit.edu>
2019-05-24 14:30:44 +0200

    wifi connection featureD

--- src/app/components/device-new-bluetooth/device-new-bluetooth.component.ts
+++ src/app/components/device-new-bluetooth/device-new-bluetooth.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ScanModel} from '../../models/scan.model';
+import {LocalStoreService} from '../../services/local-store.service';
+import {Router} from '@angular/router';
 
 declare var bluetoothSerial;
 @Component({
@@ -14,6 +16,9 @@ export class DeviceNewBluetoothComponent implements OnInit, OnDestroy {
   status = '';
   availableDevices: ScanModel[] = [];
   pairedDevices: ScanModel[] = [];
+  activeDevice;
+
+  constructor(private storage: LocalStoreService, private router: Router) {}
   ngOnInit(): void {
     this.scanForPaired();
     this.subscribeRawData();
@@ -23,8 +28,7 @@ export class DeviceNewBluetoothComponent implements OnInit, OnDestroy {
     bluetoothSerial.disconnect(
       () => {
         bluetoothSerial.unsubscribe(
-           (data) => {
-            console.log('unsubscribed');
+          () => {
           }
         );
       }
@@ -46,20 +50,42 @@ export class DeviceNewBluetoothComponent implements OnInit, OnDestroy {
 
   subscribeRawData() {
     bluetoothSerial.subscribeRawData((data) => {
-      console.log('responsereal', data);
-    });
-
-    bluetoothSerial.write('123', (res) => {
-      console.log('written', res);
+      const enc = new TextDecoder('utf-8');
+      const ip = enc.decode(data);
+      console.log('responsereal', ip);
+      this.storage.addDevice(this.activeDevice.address);
+      const deviceInfo = {
+        ip: ip,
+        mac: this.activeDevice.address,
+        deviceName: this.activeDevice.name
+      };
+      const devices = [];
+      if (!devices.find((item) => item.mac === deviceInfo.mac)) {
+        devices.push(deviceInfo);
+      }
+      this.storage.setItem('devices', JSON.stringify(devices));
+      alert('Device Connected! You can go back to main screen and start using the Memory Box');
     });
   }
 
   connectDevice(device) {
     console.log(device);
+    this.activeDevice = device;
     bluetoothSerial.connect(
-      device.address,  // device to connect to
-      this.subscribeRawData(),    // start listening if you succeed
-      alert('Unable to reach the device')    // show the error if you fail
+      device.address,
+      this.getDeviceIP(),
+      (error) => {
+        console.log('failed connectiong');
+        console.log(error);
+        alert(error);
+      }
     );
   }
+
+  getDeviceIP() {
+    bluetoothSerial.write('123', (res) => {
+      console.log('written', res);
+    });
+  }
+
 }
